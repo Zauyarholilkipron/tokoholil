1c9ec79b6e10506fd19ec49375a94229f2543543
Zauyarholilkipron <zauyarholilkipron@gmail.com>
2024-12-10 22:24:39 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Holil Cake & Bakery adalah rantai toko roti Indonesia terkemuka dengan 6 cabang (dapur pusat) di bangka belitung : Pangkal Pinang, kampak, Semabung, Air hitam, Kampung Dull, Grimaya, . Kami masih terus memperluas secara nasional ke kota-kota lain.</p>
+<p>
+Holil Cake & Bakery  adalah salah satu pelopor pertama dalam bisnis roti modern di Bangka Belitung. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #ff8680"><b>HOLIL-CAKE BAKERY</b></h3>
+					<p>Jl. Kace Timur Gang hayati No 165</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6283179116931</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> Holil-cake@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #ff8680; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Zauyar Holil Kipron</span>
+		</div>
+	</footer>
+
+</body>
+</html>
